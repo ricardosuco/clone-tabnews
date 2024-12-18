4a77459fa96d9f91454d54f6453186bf030eafe6
Ricardo Avelino <rick58@ymail.com>
2024-12-18 07:21:05 -0300

    finaliza endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum");
-  console.log(result.rows)
-  response.status(200).json({status: "API is running"});
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [process.env.POSTGRES_DATABASE],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: databaseMaxConnections.rows[0].max_connections,
+        opened_connections: databaseOpenedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.spec.js
+++ tests/integration/api/v1/status/get.spec.js
@@ -1,4 +1,25 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+  expect(new Date(responseBody.updated_at)).toBeInstanceOf(Date);
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  expect(responseBody).toMatchObject({
+    updated_at: expect.any(String),
+    dependencies: {
+      database: {
+        version: expect.any(String),
+        max_connections: expect.any(String),
+        opened_connections: expect.any(Number),
+      },
+    },
+  });
 });
