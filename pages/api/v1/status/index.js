fca330b143a954dc28d48efe9be2af3d63b79a04
Ricardo Avelino <rick58@ymail.com>
2025-12-09 22:54:17 -0300

    fix: update

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,17 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors";
 
-async function status(request, response) {
-  try {
+const router = createRouter();
+
+router.get(getHandler);
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+async function getHandler(request, response) {
     const updatedAt = new Date().toISOString();
     const databaseVersion = await database.query("SHOW server_version;");
     const databaseMaxConnections = await database.query(
@@ -23,14 +32,18 @@ async function status(request, response) {
         },
       },
     });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-    console.log("\n Erro dentro do catch do controller");
-    console.error(publicErrorObject);
-    response.status(500).json(publicErrorObject);
-  }
 }
 
-export default status;
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+  console.log("\n Erro dentro do catch do next-connect:");
+  console.error(publicErrorObject);
+  response.status(500).json(publicErrorObject);
+}
+
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
